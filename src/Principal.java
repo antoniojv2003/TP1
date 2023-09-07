84f614789b5d12fa4b7a63403fbe09f1a24b43aa
antoniojv2003 <ajv7070@gmail.com>
2023-09-07 16:08:58 -0300

    Commit para llevar al repositorio todos los archivos

---
+++ src/Principal.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author estudiante
+ */
+public class Principal {
+    public static void main(String[] args) { //CREACIÒN DEL TP CON psvm+TAB
+        //DECLARACIÒN DE VARIABLES
+        int entero;
+        float flotante;
+        double datoDouble;
+        char caract;
+        boolean booleano;
+        String cadena;
+        //INICIALIZACIÓN
+        entero=4;
+        flotante=6.2f;
+        datoDouble=1.23242; //observar diferencias con el tipo flotante
+        cadena="hola mundo"; //comillas DOBLES
+        caract='h'; //comillas SIMPLES
+        booleano=true;
+        
+        
+        //muestra por pantalla de los datos
+        
+        System.out.println(entero + " "+ flotante+" "+datoDouble+" "+cadena+" "+caract + " "+ booleano);  //el " " es para que se genere un espacio
+        
+        
+        
+        
+        //HACER LO QUE SIGUE EN EL TP
+    }
+    
+}
