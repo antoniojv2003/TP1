c8a75365412109ece2036fc9d878e3ab6a978b49
antoniojv2003 <ajv7070@gmail.com>
2023-09-11 22:25:57 -0300

    modificaciones hasta el punto 7

---
+++ src/paqClases/PrimeraClase.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paqClases;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PrimeraClase {
+    String unaCadena;  //definicion de los atributos solicidatos
+    int unEntero; 
+    
+    //definicion de metodos
+    
+    void mostrar()
+    {
+        
+    }
+    
+    public static void main(String[] args)  //main()
+    {
+        
+    }
+}
